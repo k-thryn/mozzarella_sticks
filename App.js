721f643320374979173604737c1602ca4fc69d80
Kathryn Stavish <stavish.k@husky.neu.edu>
2018-05-16 14:02:43 -0400

    anthropomorphic mozzarella stick with a bubbly personality, family and friends

--- App.js
+++ App.js
@@ -1,16 +1,41 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image, TouchableHighlight, Alert } from 'react-native';
 
 export default class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
+        <Text>mozzarella stick</Text>
+        <MozzarellaStick></MozzarellaStick>
       </View>
     );
   }
+}
+
+class MozzarellaStick extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {isGrinning: false};
+    }
+    
+    _onPress() {
+        this.setState(previousState => {
+                      return {isGrinning: !(previousState.isGrinning)};
+                      });
+    }
+    
+    _onLongPress() {
+        Alert.alert('MUUUUSSSZZZAREELL');
+    }
+    
+    render() {
+        let img = this.state.isGrinning ? require('./img/mozz-stick-grin.png') : require('./img/mozz-stick.png');
+        return (
+                <TouchableHighlight onPress={this._onPress.bind(this)} onLongPress={this._onLongPress} underlayColor="black">
+                    <Image source={img}/>
+                </TouchableHighlight>
+        );
+    }
 }
 
 const styles = StyleSheet.create({
