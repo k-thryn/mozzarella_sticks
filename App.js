5ddf40e074d6e6889a8293134b126a66d43b7cf4
Kathryn Stavish <kmstavish26@gmail.com>
2018-05-17 12:48:47 -0400

    animated map show/hide

--- App.js
+++ App.js
@@ -1,27 +1,18 @@
 import React from 'react';
-import { StyleSheet, Text, View, ScrollView, Image, Row, TouchableWithoutFeedback, Alert } from 'react-native';
+import { StyleSheet, Text, View, ScrollView, Image, Row, TouchableWithoutFeedback, Alert, Animated } from 'react-native';
 import { MapView } from 'expo';
 
 export default class App extends React.Component {
     constructor(props) {
         super(props);
         this.state = {showMap: false};
-        this.toggleShowMap = this.toggleShowMap.bind(this);
-    }
-    
-    // Handler function to allow child components to toggle showMap flag.
-    toggleShowMap() {
-        this.setState(previousState => {
-                      return {showMap: !(previousState.showMap)};
-                      });
     }
     
     render() {
         return (
                 <ScrollView contentContainerStyle={styles.container}>
                 <Text>mozzarella stick</Text>
-                <MozzarellaStick toggleShowMap={this.toggleShowMap}></MozzarellaStick>
-                <MozzarellaStickFinder showMap={this.state.showMap}></MozzarellaStickFinder>
+                <MozzarellaStick></MozzarellaStick>
                 </ScrollView>
                 );
     }
@@ -30,16 +21,14 @@ export default class App extends React.Component {
 class MozzarellaStick extends React.Component {
     constructor(props) {
         super(props);
-        this.state = {isGrinning: false};
+        this.state = {isGrinning: false, showMap: false};
     }
     
     _onPress() {
         // Make the mozzarella stick smile. :)
         this.setState(previousState => {
-                      return {isGrinning: !(previousState.isGrinning)};
+                      return {isGrinning: !(previousState.isGrinning), showMap: !(previousState.showMap)};
                       });
-        // Toggle showing the map.
-        this.props.toggleShowMap();
     }
     
     _onLongPress() {
@@ -49,9 +38,12 @@ class MozzarellaStick extends React.Component {
     render() {
         let img = this.state.isGrinning ? require('./img/mozz-stick-grin.png') : require('./img/mozz-stick.png');
         return (
+                <View>
                 <TouchableWithoutFeedback onPress={this._onPress.bind(this)} onLongPress={this._onLongPress}>
-                <Image source={img}/>
+                    <Image source={img}/>
                 </TouchableWithoutFeedback>
+                <MozzarellaStickFinder showMap={this.state.showMap}></MozzarellaStickFinder>
+                </View>
                 );
     }
 }
@@ -59,14 +51,30 @@ class MozzarellaStick extends React.Component {
 class MozzarellaStickFinder extends React.Component {
     constructor(props) {
         super(props);
+        let initialFlex = props.showMap ? 1 : 0;
+        this.state = {visible: props.showMap, flex: new Animated.Value(initialFlex)};
+    }
+    
+    componentWillReceiveProps(nextProps) {
+        if (nextProps.showMap) {
+            this.setState({ visible: true, flex: this.state.flex });
+        }
+        Animated.timing(
+                        this.state.flex,
+                        {
+                        toValue: nextProps.showMap ? 1 : 0,
+                        duration: 1200,
+                        }
+                        ).start();
     }
     
     render() {
-        if (this.props.showMap) {
+        if (this.state.visible) {
+            let { flex } = this.state;
             return (
-                    <View style={{flex: 1, position:'relative', alignSelf:'stretch'}}>
+                    <Animated.View style={{flex: flex, position: 'relative', alignSelf: 'stretch'}}>
                     <MapView style={styles.map}></MapView>
-                    </View>
+                    </Animated.View>
                     );
         }
         else {
